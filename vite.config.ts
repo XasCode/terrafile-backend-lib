6726d2377207b7e664039fd23c55b79a0ba9086c
Justin <JEStaubach@users.noreply.github.com>
2023-06-18 21:23:42 -0400

    default to not running unmocked tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,11 @@
-/// <reference types="vitest" />
+import { defineConfig, mergeConfig } from 'vitest/config';
+import viteConfig from './vite-base.config';
 
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import lodash from 'lodash';
-import dts from 'vite-plugin-dts';
-import builtinModules from 'builtin-modules';
-import pkg from './package.json';
-import commonjsExternals from 'vite-plugin-commonjs-externals';
-
-const { escapeRegExp } = lodash;
-
-const externals = [
-  ...builtinModules,
-  ...Object.keys(pkg.dependencies).map((name) => new RegExp('^' + escapeRegExp(name) + '(\\/.+)?$')),
-];
-
-export default defineConfig({
-  build: {
-    lib: {
-      name: 'terrafile-backend-lib',
-      fileName: 'terrafile-backend-lib',
-      entry: resolve(__dirname, 'src/backend/index.ts'),
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      exclude: [`__tests__/backend.unmocked.spec.ts`],
     },
-  },
-  optimizeDeps: {
-    exclude: externals as string[],
-  },
-  plugins: [
-    dts(),
-    commonjsExternals({
-      externals,
-    }),
-  ],
-  test: {
-    setupFiles: './__tests__/testUtils/testSetupFile.ts',
-    mockReset: true,
-    coverage: {
-      provider: 'istanbul',
-    },
-    environment: 'node',
-    testTimeout: 20000,
-    include: ['**/__tests__/**/*.spec.ts'],
-  },
-});
+  }),
+);
